624ca8a1f3df6cf5134805c229f55638b95364d2
Cho ByeongChan <jobc0335@gmail.com>
2018-01-16 22:28:54 +0900

    add login service

---
+++ src/main/java/com/thirty/api/service/LoginService.java
@@ -0,0 +1,20 @@
+package com.thirty.api.service;
+
+import com.thirty.api.domain.Member;
+import com.thirty.api.persistence.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ByeongChan on 2018. 1. 16..
+ */
+
+@Service
+public class LoginService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void save(Member member){ memberRepository.save(member); }
+
+    public Member findByUniqueKey(String uniqueKey){ return memberRepository.findByUniqueKey(uniqueKey); }
+}

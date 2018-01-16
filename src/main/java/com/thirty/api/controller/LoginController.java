e83370eeb84c68886243178d1e018ef819d71091
Cho ByeongChan <jobc0335@gmail.com>
2018-01-16 22:04:01 +0900

    add login controller

---
+++ src/main/java/com/thirty/api/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.thirty.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ByeongChan on 2018. 1. 16..
+ */
+
+@CrossOrigin(origins = "*")
+@Api(value = "Login API", description = "Login API", basePath = "/api/v1/login")
+@RestController
+@RequestMapping("/api/v1/login")
+public class LoginController {
+    
+}

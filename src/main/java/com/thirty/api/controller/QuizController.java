017d96492427c789f2fd7a1f3862e86619b29c64
Cho ByeongChan <jobc0335@gmail.com>
2018-01-18 16:31:30 +0900

    add quiz controller

---
+++ src/main/java/com/thirty/api/controller/QuizController.java
@@ -0,0 +1,17 @@
+package com.thirty.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ByeongChan on 2018. 1. 18..
+ */
+
+@CrossOrigin(origins = "*")
+@Api(value = "Quiz API", description = "Quiz API", basePath = "/api/v1/quiz")
+@RestController
+@RequestMapping("/api/v1/quiz")
+public class QuizController {
+}

72c33ad924831ecb605599cc286f7847dcc4cdf5
Cho ByeongChan <jobc0335@gmail.com>
2018-01-19 15:51:49 +0900

    add login controller test

---
+++ src/test/java/com/thirty/api/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package com.thirty.api;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by ByeongChan on 2018. 1. 19..
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginControllerTest {
+}

a00c7527f2a56576547ce4b997ffb57217445744
Cho ByeongChan <jobc0335@gmail.com>
2018-01-20 18:27:38 +0900

    add web socket configuration

--- src/test/java/com/thirty/api/LoginControllerTest.java
+++ src/test/java/com/thirty/api/LoginControllerTest.java
@@ -1,6 +1,10 @@
 package com.thirty.api;
 
+import com.thirty.api.domain.Member;
+import com.thirty.api.service.LoginService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,4 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginControllerTest {
+
+    @Autowired
+    private LoginService loginService;
+
+    @Test
+    public void saveTest(){
+        Member member = Member.build("uniqueTest", false);
+        loginService.save(member);
+    }
 }

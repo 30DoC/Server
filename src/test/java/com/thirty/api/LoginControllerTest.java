674166ae88ca00808b8f9b4e830743054ab5ca81
Cho ByeongChan <jobc0335@gmail.com>
2018-01-23 19:53:28 +0900

    add quiz insert test

--- src/test/java/com/thirty/api/LoginControllerTest.java
+++ src/test/java/com/thirty/api/LoginControllerTest.java
@@ -1,12 +1,15 @@
 package com.thirty.api;
 
 import com.thirty.api.domain.Member;
+import com.thirty.api.domain.Quiz;
 import com.thirty.api.service.LoginService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
 
 /**
  * Created by ByeongChan on 2018. 1. 19..
@@ -22,6 +25,18 @@ public class LoginControllerTest {
     @Test
     public void saveTest(){
         Member member = Member.build("uniqueTest1", false);
+        loginService.save(member);
+    }
+
+    @Test
+    public void insertQuiz(){
+        Member member = Member.build("uniuni", false);
+
+        Quiz quiz1 = Quiz.build("q1", false);
+        Quiz quiz2 = Quiz.build("q2", true);
+
+        member.setQuizList(Arrays.asList(quiz1, quiz2));
+
         loginService.save(member);
     }
 }

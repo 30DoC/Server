75211056a832f104d8e4942000b6194795019019
Cho ByeongChan <jobc0335@gmail.com>
2018-01-24 17:03:56 +0900

    add random sampling quiz method

--- src/main/java/com/thirty/api/controller/QuizController.java
+++ src/main/java/com/thirty/api/controller/QuizController.java
@@ -1,9 +1,15 @@
 package com.thirty.api.controller;
 
+import com.thirty.api.domain.Member;
+import com.thirty.api.domain.Quiz;
+import com.thirty.api.dto.StatusResponse;
+import com.thirty.api.service.LoginService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by ByeongChan on 2018. 1. 18..
@@ -14,4 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/quiz")
 public class QuizController {
+
+    @Autowired
+    LoginService quizService;
+
+    @ApiOperation(value = "quiz", notes = "quiz (return random quiz)")
+    @RequestMapping(value = "randomQuiz", method = RequestMethod.GET)
+    public List<Quiz> randomQuiz(){
+
+        Member member = quizService.randomSampling();
+
+        if(member == null) { // Exception 처리
+
+        }
+
+        if(member.isStatus()){
+            return StatusResponse.build(1);
+        } else{
+            return StatusResponse.build(0);
+        }
+    }
 }
